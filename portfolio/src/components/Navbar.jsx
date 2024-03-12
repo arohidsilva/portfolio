ce8ab16eece723f3bc6cee1f2c5ee83fe14dc1ce
arohidsilva <arohidsilva311@gmail.com>
2024-03-12 14:00:45 +0530

    Styles added to Navbar

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -2,13 +2,17 @@
 import React from 'react';
 import '../Stylesheets/Navbar.css';
 import { BiSolidMessage } from "react-icons/bi";
+import {Link} from "react-scroll";
 
 function Navbar() {
   return (
     <nav className='NVConatiner'>
         <img className='NVLogo' src='' alt='Logo'/>
         <div className='NVMenu'>
-
+            <Link className='NVMenuItem'>Home</Link>
+            <Link className='NVMenuItem'>About</Link>
+            <Link className='NVMenuItem'>Portfolio</Link>
+            <Link className='NVMenuItem'>Clients</Link>
         </div>
         <button className='NVContact d-flex'>
             <BiSolidMessage className='NVContactLogo'/>
