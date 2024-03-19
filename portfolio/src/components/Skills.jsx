d9869a0b4b6a61eeb159f9ebad50a531f41f5251
arohidsilva <arohidsilva311@gmail.com>
2024-03-20 00:52:22 +0530

    changes to the existing UI

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -9,35 +9,52 @@ import ImgCSS from '../assets/CSS.png'
 function Skills() {
   return (
     <section id='skills'>
-        <span className='SKTitle'>What I do</span>
-        <span className='SKPara'>I am a skilled and passionate web developer with knowledge in creating responsive and user friendly web applications. I have strong understanding of design and keen eye on detail. I am proficient in React.js, JavaScript, HTML and CSS as well as Analytical skills such as Python, SQL and Tableau.</span>
-        <div className='SKBars'>
-            <div className='SKBar'>
-                <img className='SKImg' src={ImgReact} alt='skilllogo'/>
-                <div className='SKText'>
-                    <h3>React JS</h3>
-                    <p>Demo text. Write your own content.</p>
+        <span className='SKTitle'>Skills</span>
+        <div className='SKSkills'>
+            <div className='SKSkill'>
+                <span className='SKCategory'>Languages</span>
+                <div className='SKBars'>
+                    <div className='SKBar'>
+                        <img className='SKImg' src={ImgReact} alt='skilllogo'/>
+                        <div className='SKText'>
+                            <h4>React JS</h4>
+                        </div>
+                    </div>
+                    <div className='SKBar'>
+                        <img className='SKImg' src={ImgJS} alt='skilllogo'/>
+                        <div className='SKText'>
+                            <h4>JavaScript</h4>
+                        </div>
+                    </div>
+                    <div className='SKBar'>
+                        <img className='SKImg' src={ImgHTML} alt='skilllogo'/>
+                        <div className='SKText'>
+                            <h4>HTML</h4>
+                        </div>
+                    </div>
+                    <div className='SKBar'>
+                        <img className='SKImg' src={ImgCSS} alt='skilllogo'/>
+                        <div className='SKText'>
+                            <h4>CSS</h4>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className='SKBar'>
-                <img className='SKImg' src={ImgJS} alt='skilllogo'/>
-                <div className='SKText'>
-                    <h3>JavaScript</h3>
-                    <p>Demo text. Write your own content.</p>
-                </div>
-            </div>
-            <div className='SKBar'>
-                <img className='SKImg' src={ImgHTML} alt='skilllogo'/>
-                <div className='SKText'>
-                    <h3>HTML</h3>
-                    <p>Demo text. Write your own content.</p>
-                </div>
-            </div>
-            <div className='SKBar'>
-                <img className='SKImg' src={ImgCSS} alt='skilllogo'/>
-                <div className='SKText'>
-                    <h3>CSS</h3>
-                    <p>Demo text. Write your own content.</p>
+            <div className='SKSkill'>
+                <span className='SKCategory'>Tools</span>
+                <div className='SKBars'>
+                    <div className='SKBar'>
+                        <img className='SKImg' src={ImgReact} alt='skilllogo'/>
+                        <div className='SKText'>
+                            <h3>Visual Studio</h3>
+                        </div>
+                    </div>
+                    <div className='SKBar'>
+                        <img className='SKImg' src={ImgJS} alt='skilllogo'/>
+                        <div className='SKText'>
+                            <h3>Tableau</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
